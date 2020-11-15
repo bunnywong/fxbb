db8051b4ba21366da7af5f3432531e11b1a4f4ab
BunnY <w@bunnywong.com>
2020-11-15 16:41:38 +0800

    responsive style just fine, work with mobile scroll button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,212 @@
-import React from "react"
+import React, { useRef, useState, useEffect } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import clsx from 'clsx'
+import { useLocation } from '@reach/router'
+// hero images
+import imgHero from '../../static/images/hero/forex-trader-1920x860.jpg'
+import imgHeroRtl from '../../static/images/hero/forex-trader-rtl.jpg'
+// feature images
+import imgFreeWebinars from '../../static/images/features/free-webinars.png'
+import imgLocalSeminars from '../../static/images/features/local-seminars.png'
+import imgPracticeAccount from '../../static/images/features/practice-account.png'
+// payment logo
+import imgMastro from '../../static/images/payments/maestro.png'
+import imgNeteller from '../../static/images/payments/neteller.png'
+import imgPayza from '../../static/images/payments/payza.png'
+import imgUnionPay from '../../static/images/payments/uniuon-pay.png'
+import imgWesternUnion from '../../static/images/payments/western-union.png'
+import imgMasterCard from '../../static/images/payments/master-card.png'
+import imgScril from '../../static/images/payments/scrill.png'
+import imgVisa from '../../static/images/payments/visa.png'
+import imgWebMoney from '../../static/images/payments/web-money.png'
+// style
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../static/style.css'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const myRef = useRef(null)
+  const location = useLocation()
+  const [rtl, setRtl] = useState(false)
+  const direction = rtl ? 'rtl' : 'auto'
+  const executeScroll = () => {
+    window.scrollTo({ behavior: 'smooth', top: myRef.current.offsetTop })
+  }
+  const features = [
+    {
+      imageSource: imgFreeWebinars,
+      title: 'Free Webinars',
+      content:
+        'Learn Everything you need to know about the forex market, trading strategies and how to manage risk.',
+    },
+    {
+      imageSource: imgLocalSeminars,
+      title: 'Local Seminars',
+      content:
+        'Gain market insights from industry experts and meet forex enthusiasts just like you.',
+    },
+    {
+      imageSource: imgPracticeAccount,
+      title: 'Practice Account',
+      content:
+        'Practice trading forex, gold and CFDs for as long as you like without any risk whatsoever.',
+    },
+  ]
+  const paymentImages = [
+    imgMastro,
+    imgNeteller,
+    imgPayza,
+    imgUnionPay,
+    imgWesternUnion,
+    imgMasterCard,
+    imgScril,
+    imgVisa,
+    imgWebMoney,
+  ]
+  const HeroContent = () => (
+    <div className="pt-3 text-white">
+      <h1>
+        <strong>NEW TO FOREX?</strong>
+      </h1>
+      <h2 className="text-orange">
+        <strong>Learn how to trade & increase your earning potential!</strong>
+      </h2>
+      <h3 className="mt-5">Register now and get:</h3>
+      <ul className="p-0">
+        <li>
+          <GreenTick />
+          <span className="mx-1">Free weekly webinars</span>
+        </li>
+        <li>
+          <GreenTick />
+          <span className="mx-1">A risk-free practice account</span>
+        </li>
+        <li>
+          <GreenTick />
+          <span className="mx-1">Personal, friendly support</span>
+        </li>
+      </ul>
+    </div>
+  )
+  const GreenTick = () => (
+    <img
+      src="https://www.forextime.com/sites/default/files/lp/new-homepage/green-tick-small.svg"
+      alt="tick"
+    />
+  )
+
+  useEffect(() => {
+    if (location.search === '?ltr=true') {
+      setRtl(true)
+    }
+  }, [location])
+
+  return (
+    <>
+      <div className="bbq">
+        {/* mobile */}
+        <section className="mobile hero d-sm-block d-lg-none d-xl-none text-center">
+          <div className="layer w-100 h-100" />
+          <HeroContent />
+          <div className="position-absolute w-100">
+            <Button
+              className="btn-register-now rounded-0 border-0 mx-auto text-dark mt-3 d-md-block d-lg-none mx-auto text-center"
+              onClick={executeScroll}
+            >
+              REGISTER NOW
+            </Button>
+          </div>
+          <img
+            src={rtl ? imgHeroRtl : imgHero}
+            className="hero-image"
+            alt="hero image"
+          />
+        </section>
+
+        <section
+          dir={direction}
+          className="desktop hero d-none d-sm-none d-lg-block"
+          style={{
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${rtl ? imgHeroRtl : imgHero})`,
+          }}
+        >
+          <Container>
+            <Row>
+              {/* LHS */}
+              <Col
+                dir={direction}
+                lg={{ span: 7 }}
+                className={clsx({
+                  'mt-5 text-white text-center': true,
+                  'text-lg-right': rtl,
+                  'text-lg-left': !rtl,
+                })}
+              >
+                <div className="mt-3 d-none d-lg-block">
+                  <HeroContent />
+                </div>
+              </Col>
+              {/* RHS */}
+              <Col lg={{ span: 5 }} className="mt-3 d-none d-lg-block">
+                <iframe
+                  src="https://my.forextime.com/en/identity/registration?sbt=Register%20Now&abt=Login%20to%20MyFXTM&theme=black"
+                  title="Registration"
+                  className="border-0 w-100 "
+                ></iframe>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </div>
+
+      <section className="d-md-block d-lg-none" ref={myRef}>
+        <iframe
+          src="https://my.forextime.com/en/identity/registration?sbt=Register%20Now&abt=Login%20to%20MyFXTM&theme=black"
+          title="Registration"
+          className="border-0 w-100 "
+        ></iframe>
+      </section>
+      {/* main */}
+      <Container className="mt-5">
+        {/* features */}
+        <section dir={direction}>
+          <Row className="text-center d-flex justify-content-center">
+            {features.map((item, index) => (
+              <Col key={index}>
+                <Card className="card border-0">
+                  <Card.Img
+                    variant="top"
+                    src={item.imageSource}
+                    className="feature-image d-block mx-auto"
+                  />
+                  <Card.Body>
+                    <Card.Title className="mx-auto">{item.title}</Card.Title>
+                    <Card.Text className="mx-auto">{item.content}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </section>
+        {/* payment logo */}
+        <hr />
+        <section dir={direction}>
+          <Row className="d-flex justify-content-center my-5">
+            {paymentImages.map((src, index) => (
+              <img
+                key={index}
+                src={src}
+                alt="payment logo"
+                className="p-1 payment-image "
+              />
+            ))}
+          </Row>
+        </section>
+      </Container>
+    </>
+  )
 }
